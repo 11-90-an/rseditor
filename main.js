362b8f926cdbcc97904227b00098a10bdd310122
1190an <83536263+11-90-an@users.noreply.github.com>
2022-06-22 15:03:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let graphGridThing = graphDimy - 240;
 //rounded coords
 let roundedX = 0;
 let roundedY = 0;
+let pos = 0;
 
 //gridStuff
 let xGraph = 0.6;
@@ -20,14 +21,17 @@ const gridContents = new Array(513).fill('air');;
 let selectionPanelImageOrder = 4;
 var selectedImg;
 
+let cnv;
+
 function preload() {
   imageInit();
 }
 
 function setup() {
-  createCanvas(graphDimx, graphDimy);
-  background(54, 57, 62);
-  stroke(90);
+  cnv = createCanvas(graphDimx, graphDimy);
+  cnv.background(54, 57, 62);
+  cnv.stroke(90);
+  cnv.mouseClicked(clickedMouse);
 
   gridBorders();
   gridItself();
@@ -37,23 +41,22 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    roundedX = floor(mouseX / 30);
-    roundedY = floor(mouseY / 30);
-
-    if ((mouseX > 0) && (mouseX < graphDimx - 3) && (mouseY > 0) && (mouseY < graphGridThing - 35) && mouseButton === LEFT) {
-      console.log(roundedX + " " + roundedY);
-      gridContentsManage();
-    }
-    else if ((mouseX >= 0) && (mouseX <= graphDimx - 3) && (mouseY >= graphGridThing - 10) && (mouseY <= graphDimy + 10)) {
-      console.log("selection panel");
-      selectionPanelSelectionIndicator();
-      selectionPanelImages();
-    }
-    else {
-      console.log("outside");
-    }
-  }
   gridBorders();
   gridItself();
+}
+
+function clickedMouse() {
+  roundedX = floor(mouseX / 30);
+  roundedY = floor(mouseY / 30);
+  pos = (roundedY * 27) + roundedX;
+
+  if ((mouseX > 0) && (mouseX < graphDimx - 3) && (mouseY > 0) && (mouseY < graphGridThing - 35)) {
+    console.log(roundedX + " " + roundedY);
+    gridContentsManage();
+  }
+  else {
+    console.log("selection panel");
+    selectionPanelSelectionIndicator();
+    selectionPanelImages();
+  }
 }
